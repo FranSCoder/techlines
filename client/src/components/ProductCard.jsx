4f733918fc961917bf379900be506784f9606392
FranSCoder <118598218+FranSCoder@users.noreply.github.com>
2023-03-11 18:55:11 +0100

    add cart to item and remove

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -12,11 +12,14 @@ import {
   Link,
   HStack,
   Text,
+  useToast,
 } from '@chakra-ui/react';
 import { FiShoppingCart } from 'react-icons/fi';
 import { Link as ReactLink } from 'react-router-dom';
 import { StarIcon } from '@chakra-ui/icons';
 import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addCartItem } from '../redux/actions/cartActions';
 
 const Rating = ({ rating, numberOfReviews }) => {
   const [iconSize, setIconSize] = useState('14px');
@@ -37,6 +40,22 @@ const Rating = ({ rating, numberOfReviews }) => {
 };
 
 const ProductCard = ({ product }) => {
+  const dispatch = useDispatch();
+  const toast = useToast();
+  const cartInfo = useSelector((state) => state.cart);
+  const { cart } = cartInfo;
+  const addItem = (id) => {
+    if (cart.some((cartItem) => cartItem.id === id)) {
+      toast({
+        description: 'Este producto ya está en tu carrito. Ve a tu carrito para cambiar la cantidad.',
+        status: 'error',
+        isClosable: true,
+      });
+    } else {
+      dispatch(addCartItem(id, 1));
+      toast({ description: 'Producto añadido.', status: 'success', isClosable: true });
+    }
+  };
   return (
     <Stack
       p={2}
@@ -83,7 +102,7 @@ const ProductCard = ({ product }) => {
           {product.price.toFixed(2)}
         </Box>
         <Tooltip label='Añadir al carrito' bg='white' placement='top' color='gray.800' fontSize='1.2em'>
-          <Button variant='ghost' display='flex' disabled={product.stock <= 0}>
+          <Button variant='ghost' display='flex' disabled={product.stock <= 0} onClick={() => addItem(product._id)}>
             <Icon as={FiShoppingCart} h={7} w={7} alignSelf='center' />
           </Button>
         </Tooltip>
